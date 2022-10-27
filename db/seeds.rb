b7c3b276dfcbebc3283152728faba7315ab3d325
JBE29 <jeanbaptiste.emily@gmail.com>
2022-10-27 19:01:27 +0200

    second commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,49 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+City.destroy_all
+Dogsitter.destroy_all
+Dog.destroy_all
+Stroll.destroy_all
+
+c1 = City.create!(city_name: "Quimper")
+c2 =City.create!(city_name: "Pont-l'Abbé")
+c3 = City.create!(city_name: "Bénodet")
+c4 = City.create!(city_name: "Concarneau")
+
+
+ds1 = Dogsitter.create!(ds_name: Faker::Sports::Football.player, city: c1)
+ds2 = Dogsitter.create!(ds_name: Faker::Sports::Football.player, city: c2)
+ds3 = Dogsitter.create!(ds_name: Faker::Sports::Football.player, city: c3)
+ds4 = Dogsitter.create!(ds_name: Faker::Sports::Football.player, city: c4)
+
+d1 = Dog.create!(name:Faker::Creature::Dog.name,
+        race:Faker::Creature::Dog.breed,
+        city: c1)
+d2 = Dog.create!(name:Faker::Creature::Dog.name,
+        race:Faker::Creature::Dog.breed,
+        city: c1)
+d3 = Dog.create!(name:Faker::Creature::Dog.name,
+        race:Faker::Creature::Dog.breed,
+        city: c4)
+d4 = Dog.create!(name:Faker::Creature::Dog.name,
+        race:Faker::Creature::Dog.breed,
+        city: c2)
+
+
+s1 = Stroll.create!(date:Faker::Date.in_date_period ,
+        dogsitter: ds2,
+        dog: d4)
+s2 = Stroll.create!(date:Faker::Date.in_date_period ,
+    dogsitter: ds1,
+    dog: d1)
+s3 = Stroll.create!(date:Faker::Date.in_date_period ,
+    dogsitter: ds3,
+    dog: d2)
+s4 = Stroll.create!(date:Faker::Date.in_date_period ,
+    dogsitter: ds3,
+    dog: d1)
+s5 = Stroll.create!(date:Faker::Date.in_date_period ,
+    dogsitter: ds1,
+    dog: d3)
+
+
+
